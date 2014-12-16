2bef67e26bd880a1a91d3366c32be38db09fdff6
Hayleyr <hayley.s.richardson@gmail.com>
2014-12-15 22:42:13 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -124,7 +124,7 @@ $.getJSON('councilDistv2.geojson',function(data){
    pointToLayer: function (feature, latlng) {
        return L.circleMarker(latlng, geojsonMarkerOptions);
       if (feature.property.PCC == "PCC") { geoJsonMarkerOptions.color = "#575757"; return L.circleMarker(latlng, geojsonMarkerOptions); }
-      else (feature.property.PCC == "null") { geoJsonMarkerOptions.color = "#578324"; return L.circleMarker(latlng, geojsonMarkerOptions); }
+      else (feature.property.PCC == "null") { geoJsonMarkerOptions.color = "#578324"; return L.circleMarker(latlng, geojsonMarkerOptions); 
       
 
 }).addTo(map);
